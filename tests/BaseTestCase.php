fe922da998bff95fc991c29fe74cd854de8ffeb3
thedarkprincedc <brettmosley@hotmail.com>
2018-06-03 23:33:51 -0400

    removed unnecessary code from base test case

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -7,13 +7,10 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\Environment;
 use \PHPUnit\Framework\TestCase;
-ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+// ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
 
-// require __DIR__ . '/../../src/helper_functions.php';
-// require __DIR__ . '/../../src/database_functions.php';
-// require __DIR__ . '/../../src/paramstosql.php';
 /**
  * This is an example class that shows how you could set up a method that
  * runs the application. Note that it doesn't cover all use-cases and is
@@ -77,76 +74,12 @@ class BaseTestCase extends TestCase
         $this->app = $app = new App($settings);
         // Set up dependencies
         require __DIR__ . '/../src/dependencies.php';
-        // require_once __DIR__ . '/../vendor/RedBeanPHP5_1_0/rb.php';
+
         // Register middleware
         if ($this->withMiddleware) {
             require __DIR__ . '/../src/middleware.php';
-            //require __DIR__ . '/../src/middleware/auth.middleware.php';
         }
-        // require_once __DIR__ . '/../src/controllers/romsync.controller.php';
-        // require_once __DIR__ . '/../src/controllers/gamesdb.controller.php';
-        // require_once __DIR__ . '/../src/controllers/youtube.controller.php';
-        // require_once __DIR__ . '/../src/controllers/auth.controller.php';
         // Register routes
         require __DIR__ . '/../src/routes.php';
     }
-    /**
-     * Process the application given a request method and URI
-     *
-     * @param string $requestMethod the request method (e.g. GET, POST, etc.)
-     * @param string $requestUri the request URI
-     * @param array|object|null $requestData the request data
-     * @return \Slim\Http\Response
-     */
-    // public function runApp($requestMethod, $requestUri, $requestData = null)
-    // {
-        
-    //     // Create a mock environment for testing with
-    //     $environment = Environment::mock(
-    //         [
-    //             'REQUEST_METHOD' => $requestMethod,
-    //             'REQUEST_URI' => $requestUri
-    //         ]
-    //     );
-
-    //     // Set up a request object based on the environment
-    //     $request = Request::createFromEnvironment($environment);
-
-    //     // Add request data, if it exists
-    //     if (isset($requestData)) {
-    //         $request = $request->withParsedBody($requestData);
-    //     }
-
-    //     // Set up a response object
-    //     $response = new Response();
-
-    //     // Use the application settings
-    //     $settings = require __DIR__ . '/../../src/settings.php';
-    //     require_once __DIR__ . '/../../vendor/RedBeanPHP5_1_0/rb.php';
-    //     // Instantiate the application
-    //     $app = new App($settings);
-
-    //     // Set up dependencies
-    //     require __DIR__ . '/../../src/dependencies.php';
-    //     //require __DIR__ . '/../../src/constants.php';
-    //     // Register middleware
-    //     if ($this->withMiddleware) {
-    //         require __DIR__ . '/../../src/middleware.php';
-    //         require __DIR__ . '/../../src/middleware/auth.middleware.php';
-    //     }
-    //     // require_once( __DIR__ . '/../../src/AuthController.php');
-    //     require_once( __DIR__ . '/../../src/controllers/romsync.controller.php');
-    //     require_once( __DIR__ . '/../../src/controllers/gamesdb.controller.php');
-    //     require_once( __DIR__ . '/../../src/controllers/youtube.controller.php');
-    //     require_once( __DIR__ . '/../../src/controllers/auth.controller.php');
-
-    //     // Register routes
-    //     require __DIR__ . '/../../src/routes.php';
-
-    //     // Process the application
-    //     $response = $app->process($request, $response);
-
-    //     // Return the response
-    //     return $response;
-    // }
 }
